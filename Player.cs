1d562d473d39834c46aeec7a3119a1f34764cc0c
Tatiana Cifuentes Gaitán <taciga30@gmail.com>
2024-04-25 01:18:19 -0500

    Milestone 3 development

---
+++ Player.cs
@@ -0,0 +1,24 @@
+namespace P1Guess_the_number
+{
+class Player
+{
+    public string _namePlayer;
+    public int _assumptions;
+    
+
+
+    public static int MakeGuess()
+    {
+        Console.WriteLine("→ Ingresa el número:");
+            var numPlayer = Console.ReadLine();
+            return (Int32.Parse(numPlayer));
+       
+    }
+
+    public Player(string namePlayer)
+    {
+        _namePlayer = namePlayer;
+        _assumptions = 0;
+    }
+}
+}
--- Program.cs
+++ Program.cs
@@ -1,39 +1,20 @@
-﻿namespace P1Guess_the_number;
-
-class Program
+﻿namespace P1Guess_the_number
 {
-    static void Main(string[] args)
-    {
-        int numRandom = Game.GeneratedRandom();
-        Console.WriteLine(numRandom);
-        Console.WriteLine("Ingresa tu nombre:");
-        var namePlayer = Console.ReadLine();
-        Console.WriteLine("¿Cuántos intentos quieres para poder adivinar el número?");
-        var numTry = Console.ReadLine();
-        Console.WriteLine($"Bienvenida al juego del mentalista {namePlayer}. Tendrás {numTry} oportinidades para lograr descubir el número correcto. Intenta adivinar el número");
-        for (int i = 1; i < (Int32.Parse(numTry))+1; i++)
-        {
-            Console.WriteLine("Ingresa el número:");
-            var numPlayer = Console.ReadLine();
 
-            if (Int32.Parse(numPlayer) == numRandom)
-            {
-                Console.WriteLine("Has adivinado el número");
-                break;
-            }
-            else if (Int32.Parse(numPlayer) > numRandom)
-            {
-                int intentos = Int32.Parse(numTry)-i;
-                Console.WriteLine($"El número es menor, te quedan {intentos} intentos");
-            }
-            else if (Int32.Parse(numPlayer) < numRandom)
-            {
-                int intentos = Int32.Parse(numTry)-i;
-                Console.WriteLine($"El número es mayor, te quedan {intentos} intentos");
-            }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingresa tu nombre:");
+            var namePlayer = Console.ReadLine();
+
+            var newPlayer = new Player(namePlayer);
+
+            Game game = new Game(newPlayer);
+            game.Start();
+
+            Console.WriteLine("Presiona una tecla para salir...");
+            Console.ReadKey(true);
         }
-        Console.WriteLine($"El número era {numRandom}");
-        Console.WriteLine("Presiona una tecla para salir...");
-        Console.ReadKey(true);
     }
 }
